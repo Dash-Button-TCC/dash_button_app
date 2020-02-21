deece31616938b1aa43c801ebdc6b1132d5b1d6b
Edimar Calebe  Castanho <calebe.sep@hotmail.com>
2020-02-20 23:13:42 -0300

    Initial commit.
    * Mockup application added.
    * Screenshots added.

---
+++ README.md
@@ -0,0 +1,16 @@
+# Aplicação do Dash Button
+
+Aplicação Mobile do trabalho de conclusão de curso de Edimar Calebe Castanho e Gabriel Chaves Borges.
+
+Esta aplicação está em desenvolvimento...
+
+# Screenshots
+
+| Dark Mode | Light Mode |
+|:---------:|:----------:|
+| <img src="screenshots/home_dark.png" width="400"> | <img src="screenshots/home_light.png" width="400"> |
+| <img src="screenshots/details_dark.png" width="400"> | <img src="screenshots/details_light.png" width="400"> |
+
+# Agradecimentos
+
+Esta aplicação é inspirada no conceito de aplicação de viagens do [JideGuru](https://github.com/JideGuru/FlutterTravel).
---
+++ lib/util/places.dart
@@ -0,0 +1,77 @@
+List places = [
+  {
+    "name": "Salgadinho Doritos",
+    "img": "assets/products/doritos.jpeg",
+    "price": r"R$5,00",
+    "location": "Mercado da Esquina",
+    "details": "Pellentesque in ipsum id orci porta dapibus. "
+        "Nulla porttitor accumsan tincidunt. Donec rutrum "
+        "congue leo eget malesuada. "
+        "\n\nPraesent sapien massa, convallis a pellentesque "
+        "nec, egestas non nisi. Donec rutrum congue leo eget malesuada. "
+        "Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. "
+        "Sed porttitor lectus nibh. Donec sollicitudin molestie malesuada. "
+        "\nCurabitur arcu erat, accumsan id imperdiet et, porttitor at sem. "
+        "Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui.",
+  },
+  {
+    "name": "Refrigerante Coca Cola",
+    "img": "assets/products/cocacola.jpeg",
+    "price": r"R$6,00",
+    "location": "Mercado da Esquina",
+    "details": "Pellentesque in ipsum id orci porta dapibus. "
+        "Nulla porttitor accumsan tincidunt. Donec rutrum "
+        "congue leo eget malesuada. "
+        "\nPraesent sapien massa, convallis a pellentesque "
+        "nec, egestas non nisi. Donec rutrum congue leo eget malesuada. "
+        "Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. "
+        "Sed porttitor lectus nibh. Donec sollicitudin molestie malesuada. "
+        "\nCurabitur arcu erat, accumsan id imperdiet et, porttitor at sem. "
+        "Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui.",
+  },
+  {
+    "name": "Detergente Ype",
+    "img": "assets/products/ype.jpg",
+    "price": r"R$3,10",
+    "location": "Mercado da Esquina",
+    "details": "Pellentesque in ipsum id orci porta dapibus. "
+        "Nulla porttitor accumsan tincidunt. Donec rutrum "
+        "congue leo eget malesuada. "
+        "\n\nPraesent sapien massa, convallis a pellentesque "
+        "nec, egestas non nisi. Donec rutrum congue leo eget malesuada. "
+        "Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. "
+        "Sed porttitor lectus nibh. Donec sollicitudin molestie malesuada. "
+        "\nCurabitur arcu erat, accumsan id imperdiet et, porttitor at sem. "
+        "Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui.",
+  },
+  {
+    "name": "Shampoo Seda",
+    "img": "assets/products/seda.jpeg",
+    "price": r"R$10,00",
+    "location": "Mercado da Esquina",
+    "details": "Pellentesque in ipsum id orci porta dapibus. "
+        "Nulla porttitor accumsan tincidunt. Donec rutrum "
+        "congue leo eget malesuada. "
+        "\nPraesent sapien massa, convallis a pellentesque "
+        "nec, egestas non nisi. Donec rutrum congue leo eget malesuada. "
+        "Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. "
+        "Sed porttitor lectus nibh. Donec sollicitudin molestie malesuada. "
+        "\nCurabitur arcu erat, accumsan id imperdiet et, porttitor at sem. "
+        "Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui.",
+  },
+  {
+    "name": "Sabão em pó Omo",
+    "img": "assets/products/omo.jpeg",
+    "price": r"$12,00",
+    "location": "Mercado da Esquina",
+    "details": "Pellentesque in ipsum id orci porta dapibus. "
+        "Nulla porttitor accumsan tincidunt. Donec rutrum "
+        "congue leo eget malesuada. "
+        "\nPraesent sapien massa, convallis a pellentesque "
+        "nec, egestas non nisi. Donec rutrum congue leo eget malesuada. "
+        "Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. "
+        "Sed porttitor lectus nibh. Donec sollicitudin molestie malesuada. "
+        "\nCurabitur arcu erat, accumsan id imperdiet et, porttitor at sem. "
+        "Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui.",
+  },
+];
